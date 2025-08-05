54b47a1c2754e9cafde9171f1200b651167eacfe
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-05 16:46:32 -0700

    pc - migrate all files from frontend/src to vite-project/src

---
+++ vite-project/src/main/utils/currentUser.js
@@ -0,0 +1,50 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export function useCurrentUser() {
+  let rolesList = ["ERROR_GETTING_ROLES"];
+  const queryResults = useQuery({
+    queryKey: ["current user"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get("/api/currentUser");
+        try {
+          rolesList = response.data.roles.map((r) => r.authority);
+        } catch (e) {
+          console.error("Error getting roles: ", e);
+        }
+        response.data = { ...response.data, rolesList: rolesList };
+        return { loggedIn: true, root: response.data };
+      } catch (e) {
+        if (e.status === 403) {
+          return { loggedIn: false, root: {} };
+        } else {
+          console.error("Error invoking axios.get: ", e);
+          throw e;
+        }
+      }
+    },
+    initialData: { loggedIn: false, root: null, initialData: true },
+  });
+  return queryResults.data;
+}
+
+export function useLogout() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const mutation = useMutation({
+    mutationFn: async () => {
+      await axios.post("/logout");
+      await queryClient.resetQueries({ queryKey: ["current user"] });
+      navigate("/");
+    },
+  });
+  return mutation;
+}
+
+export function hasRole(currentUser, role) {
+  if (currentUser == null) return false;
+
+  return currentUser.root?.rolesList?.includes(role);
+}
---
+++ vite-project/src/main/utils/systemInfo.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export function useSystemInfo() {
+  return useQuery({
+    queryKey: ["systemInfo"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get("/api/systemInfo");
+        return response.data;
+      } catch (e) {
+        console.error("Error invoking axios.get: ", e);
+        return {};
+      }
+    },
+    initialData: {
+      initialData: true,
+      springH2ConsoleEnabled: false,
+      showSwaggerUILink: false,
+    },
+  });
+}
