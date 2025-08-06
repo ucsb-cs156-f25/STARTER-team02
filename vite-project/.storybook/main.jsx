e837acc2497ef634f0f4c19aa4a45f196b87f884
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-05 23:07:08 -0700

    pc - update for storybook

--- vite-project/.storybook/main.jsx
+++ vite-project/.storybook/main.jsx
@@ -2,15 +2,11 @@
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
+    ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-vite', // Change this line
     options: {},
   },
   staticDirs: ["../public"],
