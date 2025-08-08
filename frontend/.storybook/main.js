bc0d2536461e82db1600ed73caa77adc9018cf7c
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-07 21:06:02 -0700

    pc - add     "@storybook/addon-essentials",

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -3,6 +3,7 @@ const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-essentials",
   ],
   framework: {
     name: '@storybook/react-vite',
