560f2c0fac8f73a5e02ed79c01efef0881d7797b
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-07 21:03:28 -0700

    renamed:    .storybook/main.jsx -> .storybook/main.js

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,12 +1,11 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@chromatic-com/storybook",
-    ],
+  ],
   framework: {
-    name: '@storybook/react-vite', 
+    name: '@storybook/react-vite',
     options: {},
   },
   staticDirs: ["../public"],
