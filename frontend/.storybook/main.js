52eae79f3160ec876255a4ca6c1df3cc177fd677
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-10 20:25:20 -0700

    pc - remove configuration for storybook dependency that's no longer in package.json

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -3,7 +3,6 @@ const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
   ],
   framework: {
     name: '@storybook/react-vite',
